5ac39ff0808f9fde2252dc8dab9564734c35d86d
Arpan Shah <ap.59.shah@gmail.com>
2020-04-13 14:39:12 -0400

    added plugin for styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-typescript`    
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`    
   ]
 }
